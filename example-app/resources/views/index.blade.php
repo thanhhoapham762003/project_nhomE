74f5faf1e99e442a32f5f6f582824035aaf377d7
hoa <thanhhoapham762003@gmail.com>
2025-04-11 16:09:42 +0700

    Create database nhome and create home,index web

---
+++ example-app/resources/views/index.blade.php
@@ -0,0 +1,242 @@
+@extends('home')
+@section('content-index')
+<div class="maincontent-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="latest-product">
+                    <h2 class="section-title">Latest Products</h2>
+                    <div class="product-carousel">
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="img/iphone14pro.png" alt="" class="img-product">
+                                <div class="product-hover">
+                                    <a href="cart" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                            <a href="" class="view-details-link"><i
+                                            class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+                            <h2><a href="">Name</a></h2>
+                        
+                            <div class="product-carousel-price">
+                                10000000 vnd
+                            </div>
+                        </div>
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="img/iphone14pro.png" alt="" class="img-product">
+                                <div class="product-hover">
+                                    <a href="cart" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                            <a href="" class="view-details-link"><i
+                                            class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+                            <h2><a href="">Name</a></h2>
+                        
+                            <div class="product-carousel-price">
+                                10000000 vnd
+                            </div>
+                        </div>
+                        <div class="single-product">
+                            <div class="product-f-image">
+                                <img src="img/iphone14pro.png" alt="" class="img-product">
+                                <div class="product-hover">
+                                    <a href="cart" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Add to cart</a>
+                                            <a href="" class="view-details-link"><i
+                                            class="fa fa-link"></i> See details</a>
+                                </div>
+                            </div>
+                            <h2><a href="">Name</a></h2>
+                        
+                            <div class="product-carousel-price">
+                                10000000 vnd
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End main content area -->
+
+<div class="brands-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="brand-wrapper">
+                    <div class="brand-list">
+                        <a href="">  <img src="img/nokia.png"  style="width: 180px; height: 100px; margin-right: 10px;" alt=""></a>
+                        <a href="">  <img src="img/samsung.png"  style="width: 180px; height: 100px; margin-right: 10px;" alt=""></a>
+                        <a href="">  <img src="img/sony.png"  style="width: 180px; height: 100px; margin-right: 10px;" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End brands area -->
+
+
+
+
+
+<div class="product-widget-area">
+    <div class="zigzag-bottom"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="single-product-widget">
+                    <h2 class="product-wid-title">Top Sellers</h2>
+                    <div class="single-wid-product">
+                        <a href=""><img src="img/iphone14pro.png" alt=""
+                                class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            100000 vnd
+                        </div>
+                    </div>
+                    <div class="single-wid-product">
+                        <a href=""><img src="img/iphone14pro.png" alt=""
+                                class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            100000 vnd
+                        </div>
+                    </div>
+                    <div class="single-wid-product">
+                        <a href=""><img src="img/iphone14pro.png" alt=""
+                                class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            100000 vnd
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="single-product-widget">
+                    <h2 class="product-wid-title">Recently Viewed</h2>
+                    <div class="single-wid-product">
+                        <a href=""><img src="img/iphone14pro.png" alt=""
+                                class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            1000000 vnd
+                            
+                        </div>
+                    </div>
+                    <div class="single-wid-product">
+                        <a href=""><img src="img/iphone14pro.png" alt=""
+                                class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            1000000 vnd
+                        </div>
+                    </div>
+                    <div class="single-wid-product">
+                        <a href=""><img src="img/iphone14pro.png" alt=""
+                                class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            1000000 vnd
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="single-product-widget">
+                    <h2 class="product-wid-title">Top New</h2>
+                    <div class="single-wid-product">
+                        <a href=""><img
+                                src="img/iphone14pro.png" alt="" class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            100000000 vnđ
+                        </div>
+                    </div>
+                    <div class="single-wid-product">
+                        <a href=""><img
+                                src="img/iphone14pro.png" alt="" class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            100000000 vnđ
+                        </div>
+                    </div>
+                    <div class="single-wid-product">
+                        <a href=""><img
+                                src="img/iphone14pro.png" alt="" class="product-thumb"></a>
+                        <h2><a href="">Name</a></h2>
+                        <div class="product-wid-rating">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                        </div>
+                        <div class="product-wid-price">
+                            100000000 vnđ
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- End product widget area -->
+@endsection
